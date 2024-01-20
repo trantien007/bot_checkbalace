6a0425c5533caa74c97da5e152b8987b8cde2e8c
sssszzzz <eaaaa@gmail.com>
2024-01-20 11:32:16 +0700

    update

--- xrpsAPI.js
+++ xrpsAPI.js
@@ -1,30 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 
 async function xrpsAPI(chatId, ownerAddresses, bot, key) {
   let tickInscriptionCount = {};
   let totalInscriptionCount = 0;
 
+  async function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
   for (const ownerAddress of ownerAddresses) {
     const apiURL = `https://api.xrpscript.com/balance/${ownerAddress}`;
-    const params = {
-      // start: 1,
-      // limit: 100,
-      // chainId: 321,
-      // owner: ownerAddress
-    };
-
     try {
-      const response = await axios.get(apiURL, { params });
+      const response = await axios.get(apiURL);
 
       if (response.status === 200) {
         const data = response.data;
         const item = data || [];
-        const tick = 'XRPS';
+        const tick = "XRPS";
         let inscriptionCount = parseInt(item.result.balance || 0);
 
         inscriptionCount = inscriptionCount / 1000000;
 
-        tickInscriptionCount[tick] = (tickInscriptionCount[tick] || 0) + inscriptionCount;
+        tickInscriptionCount[tick] =
+          (tickInscriptionCount[tick] || 0) + inscriptionCount;
         totalInscriptionCount += inscriptionCount;
 
         if (!item) {
@@ -32,20 +30,22 @@ async function xrpsAPI(chatId, ownerAddresses, bot, key) {
         }
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
+
+    // Chờ 2 giây trước khi thực hiện yêu cầu tiếp theo
+    await delay(1500);
   }
 
-  let tableContent = '';
+  let tableContent = "";
   for (const tick in tickInscriptionCount) {
     tableContent += `${tick}\t|\t${tickInscriptionCount[tick]}\n`;
   }
 
-  const tableHeader = 'XRPS\t|\tTotal Inscription Count\n';
+  const tableHeader = "XRPS\t|\tTotal Inscription Count\n";
   const table = `\`\`\`\n${tableHeader}${tableContent}\nTotal\t|\t${totalInscriptionCount}\n\`\`\``;
 
-  bot.sendMessage(chatId, table, { parse_mode: 'Markdown' });
+  bot.sendMessage(chatId, table, { parse_mode: "Markdown" });
 }
 
 export default xrpsAPI;
-2
